556dde90b67dfea914d9550e4647f70cd088b089
Alex Angelov <aj.angelov@gmail.com>
2021-03-20 20:40:29 +0200

    Initial commit

---
+++ src/main/java/sap/faculty_management_system/FacultyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package sap.faculty_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacultyManagementSystemApplication.class, args);
+    }
+
+}
